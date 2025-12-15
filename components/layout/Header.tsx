3818dacb4ac2bd02f3436c802d93484d14de2e7e
JFEspanolito <software.jf.revisit192@passinbox.com>
2025-12-15 16:21:57 -0600

    Change header menu buttons text for more place holder

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -10,16 +10,16 @@ import ThemeToggle from "@/components/buttons/ThemeToggle";
 
 const links = [
   {
-    href: "/#pricing",
-    label: "Pricing",
+    href: "/#Menu1",
+    label: "Menu 1",
   },
   {
-    href: "/#testimonials",
-    label: "Reviews",
+    href: "/#Menu2",
+    label: "Menu 2",
   },
   {
-    href: "/#faq",
-    label: "FAQ",
+    href: "/#Menu3",
+    label: "Menu 3",
   },
 ];
 
@@ -181,7 +181,7 @@ export function Header() {
             {/* Your CTA on small screens */}
             <div className="flex flex-col gap-3">
               <ThemeToggle />
-              <SignInModalButton label="Get Started" />
+              <SignInModalButton label="Log In" />
             </div>
           </div>
         </div>
