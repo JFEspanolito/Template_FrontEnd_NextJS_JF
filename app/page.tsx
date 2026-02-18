747a2f38ad1edffbb75a4eb395839240fc3b75df
JFEspanolito <software.jf.revisit192@passinbox.com>
2026-02-18 13:45:58 -0600

    Page.tsx now is server side render

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,6 @@
-"use client";
-
-import { useRouter } from "next/navigation";
 import configProject from "@/data/configProject";
 
 export default function Home() {
-  const router = useRouter();
-
-  const handleExperienceClick = () => {
-    router.push("/Experience");
-  };
-
   return (
     <div
       className="relative flex min-h-screen items-center justify-center font-sans"
